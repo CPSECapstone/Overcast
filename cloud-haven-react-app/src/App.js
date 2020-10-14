d8b6a27d2e12ec3c2d8d1f21f6e9645fbe58d410
Michelle Jakab <mjakab@calpoly.edu>
2020-10-14 03:01:02 -0700

    Switch From 2 Space Indents to 3 Spaces

--- cloud-haven-react-app/src/App.js
+++ cloud-haven-react-app/src/App.js
@@ -3,15 +3,15 @@ import { Home } from './components/components'
 import './App.css';
 
 function App() {
-  return (
-    <div id="app">
-      Insert Header Here
-      <div id="page">
-        Insert Page Switching Logic Here
-        <Home />
+   return (
+      <div id="app">
+         Insert Header Here
+         <div id="page">
+         Insert Page Switching Logic Here
+         <Home />
+         </div>
       </div>
-    </div>
-  );
+   );
 }
 
 export default App;
