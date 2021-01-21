07a2cc90f8655a4c2a1102d14c6771094645f368
Gwendolyn Kidd <glkidd@calpoly.edu>
2021-01-21 11:17:57 -0800

    removed duplicate line in components file

--- cloud-haven-react-app/src/components/components.js
+++ cloud-haven-react-app/src/components/components.js
@@ -4,5 +4,4 @@ export {default as CustomForm} from './CustomForm/CustomForm';
 export {default as SignUp} from './SignUp/SignUp';
 export {default as TopBar} from './TopBar/TopBar';
 export {default as ProfileDropdown} from './ProfileDropdown/ProfileDropdown'
-export {default as SignUp} from './SignUp/SignUp';
 export {default as TabSystem} from './TabSystem/TabSystem';
