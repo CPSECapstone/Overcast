d63fcae99d09eabc41b749f110fad2ef9d359175
Michelle Jakab <mjakab@calpoly.edu>
2021-02-01 02:24:53 -0800

    Re-style the Profile Dropdown
    Make it pretty much match the figma. It doesn't match 100% exactly, but
    it's pretty close.

--- cloud-haven-react-app/src/components/ProfileDropdown/ProfileDropdown.js
+++ cloud-haven-react-app/src/components/ProfileDropdown/ProfileDropdown.js
@@ -1,27 +1,38 @@
 import React from 'react';
 import Dropdown from 'react-bootstrap/Dropdown'
 import profile from "../../images/profile-user.png";
+import settings from "../../images/settings.png";
+import logout from "../../images/logout.png";
 import './ProfileDropdown.css';
 
 const ProfileDropdown = () => {
 
     return (
-        <Dropdown>
+        <Dropdown menuAlign="right">
             <Dropdown.Toggle variant="light">
-                <span>Username</span>
+                <span id="DropdownUsername">Username</span>
             </Dropdown.Toggle>
 
             <Dropdown.Menu>
                 <Dropdown.Item href="/profile">
-                    <span className="DropdownItem">Profile</span>
+                    <span className="DropdownItem">
+                        Profile
+                        <img className="Icon" src={profile} />
+                    </span>
                 </Dropdown.Item>
                 <Dropdown.Divider />
                 <Dropdown.Item href="#/settings">
-                    <span className="DropdownItem">Settings</span>
+                    <span className="DropdownItem">
+                        Settings
+                        <img className="Icon" src={settings} />
+                    </span>
                 </Dropdown.Item>
                 <Dropdown.Divider />
                 <Dropdown.Item href="#/signOut">
-                    <span className="DropdownItem">Sign Out</span>
+                    <span className="DropdownItem">
+                        Sign Out
+                        <img className="Icon" src={logout} />
+                    </span>
                 </Dropdown.Item>
             </Dropdown.Menu>
         </Dropdown>
