f42f935b9e55e501eb1625485a3a0292e5bdde9a
[Shreya Tumma] <ShreyaTumma@users.noreply.github.com>
2021-02-16 17:51:31 -0800

    stashing files

--- cloud-haven-react-app/src/components/Sidebar/Sidebar.js
+++ cloud-haven-react-app/src/components/Sidebar/Sidebar.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState} from 'react';
 import './Sidebar.css';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {faBars, faHome} from "@fortawesome/free-solid-svg-icons";
@@ -11,6 +11,7 @@ function Sidebar() {
     const [isSidebarOpen, setIsSidebarOpen] = useState(false);
     const showSidebar = () => setIsSidebarOpen(!isSidebarOpen);
     const className = isSidebarOpen ? "sidebar sidebar-open" : "sidebar";
+
     return (
         <div>
             <div class={className}>
