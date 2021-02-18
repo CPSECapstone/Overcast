8934d20454a80a393d1882e940f40d91ad61981c
[Shreya Tumma] <ShreyaTumma@users.noreply.github.com>
2021-02-18 11:35:01 -0800

    selected styling

--- cloud-haven-react-app/src/components/Sidebar/Sidebar.js
+++ cloud-haven-react-app/src/components/Sidebar/Sidebar.js
@@ -1,29 +1,37 @@
 import React, {useState} from 'react';
 import './Sidebar.css';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {faBars, faHome} from "@fortawesome/free-solid-svg-icons";
-
+import {faBars, faHome, faQuestion} from "@fortawesome/free-solid-svg-icons";
 
 const homeIcon = <FontAwesomeIcon icon={faHome} />
 const barIcon = <FontAwesomeIcon icon={faBars}/>
+const questionMarkIcon = <FontAwesomeIcon icon={faQuestion}/>
+
 
 function Sidebar(props) {
+    const setSelected = () => {
+        
+    }
     return (
         <div>
             <div class={props.className}>
             <span class="barIcon"><i className="fas fa-bars" onClick={props.showSidebar} style={{ fontSize: '1.75em' }}>{barIcon}</i></span>
                 <ul class="ul">
                     <li class="list">
-                        <a class="items" href="/">
-                            <span class="icon"><i className="fa fa-fw fa-home" style={{ fontSize: '1.75em' }}>{homeIcon}</i></span>
-                            <span class="title">Home</span>
+                        <a class="item" href="/" onClick={setSelected}>
+                            <span class="sidebar-selected-color circle-icon-selected icon"><i className="fa fa-fw fa-home" style={{ fontSize: '1.75em' }}>{homeIcon}</i></span>
+                            <span class="sidebar-selected-color rectangle title">Home</span>
+                        </a>
+                        <a class="item" href="/test">
+                            <span class="sidebar-app-color circle-icon icon"><i className="fa fa-fw fa-question" style={{ fontSize: '1.75em' }}>{questionMarkIcon}</i></span>
+                            <span class="title">Test Application</span>
                         </a>
                     </li>
                 </ul>
             </div>
         </div>
     );
- }
+}
  
- export default Sidebar;
+export default Sidebar;
  
