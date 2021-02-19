5ca8043ca0d838c7257df29ae1e01649ae708a33
Michelle Jakab <mjakab@calpoly.edu>
2021-02-18 17:03:56 -0800

    Create & Use `FileItem` Component
    Create a `FileItem` component that will handle the styling of each item
    in the file list. In the future, this will also handle the functionality
    of switching between files/forms, so lay the groundwork for that.

--- cloud-haven-react-app/src/components/FileList/FileList.js
+++ cloud-haven-react-app/src/components/FileList/FileList.js
@@ -5,9 +5,38 @@ export default props => {
    return (
       <div id="FileList">
          <h2 id="FileListTitle">File List</h2>
-         <div className="ListItem">list item</div>
-         <div className="ListItem">list item</div>
-         <div className="ListItem">list item</div>
+         <FileItem
+            itemId={1}
+            title="New Patient Waiver"
+            datePosted="11/12/20"
+            dueDate="12/31/20"
+            onClick={() => {console.log("temp onClick function");}}
+         />
+         <FileItem
+            itemId={2}
+            title="Health Insurance"
+            datePosted="11/12/20"
+            dueDate="12/09/20"
+            onClick={() => {console.log("temp onClick function");}}
+         />
+         <FileItem
+            itemId={1}
+            title="New Patient Waiver"
+            datePosted="11/11/20"
+            dueDate="12/9/20"
+            onClick={() => {console.log("temp onClick function");}}
+         />
       </div>
    )
+}
+
+const FileItem = (props) => {
+   const { itemId, title, datePosted, dueDate, onClick } = props;
+   return (
+      <button className="FileItem" onClick={onClick}>
+         <h3 className="ItemTitle">{title}</h3>
+         <div className="DateField">Posted: {datePosted}</div>
+         <div className="DateField">Posted: {dueDate}</div>
+      </button>
+   );
 }
