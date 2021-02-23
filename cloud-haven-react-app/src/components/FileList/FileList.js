137fedf0474a55c0855eb7eb542aee076f57d391
Michelle Jakab <mjakab@calpoly.edu>
2021-02-23 12:14:39 -0800

    Swap Unused `props` for `()`

--- cloud-haven-react-app/src/components/FileList/FileList.js
+++ cloud-haven-react-app/src/components/FileList/FileList.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './FileList.css';
 
-export default props => {
+export default () => {
    return (
       <ul id="FileList">
          <h2 id="FileListTitle">File List</h2>
