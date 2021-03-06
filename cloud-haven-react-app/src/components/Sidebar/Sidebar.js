bda89fb2920928f34a90b4a564cd6a4e9176835a
[Shreya Tumma] <ShreyaTumma@users.noreply.github.com>
2021-03-06 03:21:28 -0800

    links added to list items

--- cloud-haven-react-app/src/components/Sidebar/Sidebar.js
+++ cloud-haven-react-app/src/components/Sidebar/Sidebar.js
@@ -7,15 +7,29 @@ const homeIcon = <FontAwesomeIcon icon={faHome} />
 const barIcon = <FontAwesomeIcon icon={faBars}/>
 const questionMarkIcon = <FontAwesomeIcon icon={faQuestion}/>
 
-
-
 const Sidebar = () => {
     const [isSidebarOpen, setIsSidebarOpen] = useState(false);
     const showSidebar = () => setIsSidebarOpen(!isSidebarOpen);
-    const className = isSidebarOpen ? "sidebar sidebar-open" : "sidebar";
+    const className = isSidebarOpen ? "sidebar sidebar-open" : "sidebar sidebar-closed";
+    //const [isAppSelected, setIsAppSelected] = useState(false);
+
     return(
         <div className={className}>
-            <button id="Hamburger" className="barIcon" onClick={showSidebar}><i className="fas fa-bars">{barIcon}</i></button>
+            <button id="Hamburger" onClick={showSidebar}><i className="fas fa-bars">{barIcon}</i></button>
+            <ul>
+                <li >
+                    <a href="/">
+                        <span id="AppIcon" ><i className="fa fa-home fa-lg fa-fw ">{homeIcon}</i></span>
+                        <span id="AppName">Home</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="/test">
+                        <span id="AppIcon"><i className="fa fa-home fa-lg fa-fw">{questionMarkIcon}</i></span>
+                        <span id="AppName">TestApplication</span>
+                    </a>
+                </li>
+            </ul>
         </div>
 
     )
