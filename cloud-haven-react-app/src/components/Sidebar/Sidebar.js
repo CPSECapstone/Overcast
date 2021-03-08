69b52aa20e171f1505957ccf9c2c956f1a4f9b8c
[Shreya Tumma] <ShreyaTumma@users.noreply.github.com>
2021-03-07 19:29:16 -0800

    highlighted selected app

--- cloud-haven-react-app/src/components/Sidebar/Sidebar.js
+++ cloud-haven-react-app/src/components/Sidebar/Sidebar.js
@@ -1,38 +1,72 @@
 import React, { useState } from "react";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+    useParams,
+    NavLink
+  } from "react-router-dom";
 import './Sidebar.css';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {faBars, faHome, faQuestion} from "@fortawesome/free-solid-svg-icons";
+import Home from "../Home/Home";
 
 const homeIcon = <FontAwesomeIcon icon={faHome} />
 const barIcon = <FontAwesomeIcon icon={faBars}/>
 const questionMarkIcon = <FontAwesomeIcon icon={faQuestion}/>
 
+const menuListItemsConfig = [
+    {iconPath:homeIcon, urlPath: "/", listID: "Home"},
+    {iconPath: questionMarkIcon, urlPath: "/Test", listID: "TestApplication"}
+];
+
+const renderSidebarMenuOption = (listID, iconPath, urlPath, selected) => {
+
+    var classNameIcon = "";
+    var className = "";
+
+    if(selected){
+        classNameIcon = "current AppIcon";
+        className = "current current-name rectangle AppName";
+    }else {
+        classNameIcon = "icon-color AppIcon";
+        className = "rectangle AppName";
+    }
+    
+    return(
+
+    <li key={listID} id={listID}>
+        <a href={urlPath} >
+            <span className={classNameIcon}><i className="fa fa-home fa-lg fa-fw ">{iconPath}</i></span>
+            <span className={className}>{listID}</span>
+        </a>
+    </li>
+    
+    );
+}
+
 const Sidebar = () => {
+    const menuItems = [];
+    const currentPath = window.location.pathname; 
+
+    for (const itemConfig of menuListItemsConfig){
+        menuItems.push(renderSidebarMenuOption(itemConfig.listID, itemConfig.iconPath, itemConfig.urlPath, currentPath===itemConfig.urlPath));
+    }
+
     const [isSidebarOpen, setIsSidebarOpen] = useState(false);
     const showSidebar = () => setIsSidebarOpen(!isSidebarOpen);
     const className = isSidebarOpen ? "sidebar sidebar-open" : "sidebar sidebar-closed";
-    //const [isAppSelected, setIsAppSelected] = useState(false);
 
     return(
         <div className={className}>
             <button id="Hamburger" onClick={showSidebar}><i className="fas fa-bars">{barIcon}</i></button>
             <ul>
-                <li >
-                    <a href="/">
-                        <span id="AppIcon" ><i className="fa fa-home fa-lg fa-fw ">{homeIcon}</i></span>
-                        <span id="AppName">Home</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="/test">
-                        <span id="AppIcon"><i className="fa fa-home fa-lg fa-fw">{questionMarkIcon}</i></span>
-                        <span id="AppName">TestApplication</span>
-                    </a>
-                </li>
+                {menuItems}
             </ul>
-        </div>
 
-    )
+        </div>
+    );
 }
 
 export default Sidebar;
