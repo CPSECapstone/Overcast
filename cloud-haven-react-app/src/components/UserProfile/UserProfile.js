a80f20db99017f0dd8f16ba42d204df49bdd374f
patrickbartlett <patrickbartlett@me.com>
2021-03-11 11:18:13 -0800

    Pulling user info from frontend dummy data

--- cloud-haven-react-app/src/components/UserProfile/UserProfile.js
+++ cloud-haven-react-app/src/components/UserProfile/UserProfile.js
@@ -12,8 +12,12 @@ const UserProfile = () => {
     const [ editing, setEditing ] = React.useState(false);
 
     // TO DO: pull user info from passport/backend
-    const [ firstName, setFirstName ] = React.useState('Johnald')
-
+    const [ firstName, setFirstName ] = React.useState('Johnald');
+    const [ lastName, setLastName ] = React.useState('Testman');
+    const [ email, setEmail ] = React.useState('test@test.gov');
+    const [ phoneNumber, setPhoneNumber ] = React.useState('555-555-5555');
+    const [ birthDate, setBirthDate ] = React.useState('2021-01-01');
+    
     const [ firstNameFormData, setFirstNameFormData ] = React.useState({
         controlId: 'formFirstName',
         label: 'First Name',
@@ -24,28 +28,24 @@ const UserProfile = () => {
         controlId: 'formlastName',
         label: 'Last Name',
         type: 'name',
-        value: 'Lastname',
         focus: null
     })
     const [ emailFormData, setEmailFormData ] = React.useState({
         controlId: 'formEmail',
         label: 'Email Address',
         type: 'email',
-        value: 'test@test.gov',
         focus: null
     })
     const [ phoneFormData, setPhoneFormData ] = React.useState({
         controlId: 'formPhone',
         label: 'Phone Number',
         type: 'tel',
-        value: '555-555-555',
         focus: null
     })
     const [ birthFormData, setBirthFormData ] = React.useState({
         controlId: 'formBirth',
         label: 'Date of Birth',
         type: 'date',
-        value: '01-01-2000',
         focus: null
     })
 
@@ -56,7 +56,7 @@ const UserProfile = () => {
     const ProfileHeader = () => {
         return (
             <Container className='d-flex align-items-start justify-content-between'>
-                <img src={invisible} height="75" />
+                <img src={invisible} height="130" />
                 <img src={profile} height="150" />
                 <EditButton/>
             </Container>
@@ -88,41 +88,69 @@ const UserProfile = () => {
                                                 type={firstNameFormData.type}
                                                 value={firstName}
                                                 onChange={e => setFirstName(e.target.value)}
-                                                disabled={editing} />
+                                                disabled={!editing} />
                                         </Form.Group>
                                     </Col>
 
                                     <Col>
-                                        <Form.Group controlId="formLastName">
-                                            <Form.Label>Last Name</Form.Label>
-                                            <Form.Control type="name" defaultValue='Testman' disabled={!editing} />
+                                        <Form.Group controlId={lastNameFormData.controlId}>
+                                            <Form.Label>{lastNameFormData.label}</Form.Label>
+                                            <Form.Control
+                                                type={lastNameFormData.type}
+                                                value={lastName}
+                                                onChange={e => setLastName(e.target.value)}
+                                                disabled={!editing} />
                                         </Form.Group>
                                     </Col>
                                 </Row>
 
                                 <Row>
                                     <Col>
-                                        <Form.Group controlId="formEmail">
-                                            <Form.Label>Email address</Form.Label>
-                                            <Form.Control type="email" defaultValue='test@test.com' disabled={!editing} />
+                                        <Form.Group controlId={emailFormData.controlId}>
+                                            <Form.Label>{emailFormData.label}</Form.Label>
+                                            <Form.Control
+                                                type={emailFormData.type}
+                                                value={email}
+                                                onChange={e => setEmail(e.target.value)}
+                                                disabled={!editing} />
                                         </Form.Group>
                                     </Col>
 
                                     <Col>
-                                        <Form.Group controlId="formPhone">
-                                            <Form.Label>Phone Number</Form.Label>
-                                            <Form.Control type="tel" defaultValue='555-555-5555' disabled={!editing} />
+                                        <Form.Group controlId={phoneFormData.controlId}>
+                                            <Form.Label>{phoneFormData.label}</Form.Label>
+                                            <Form.Control
+                                                type={phoneFormData.type}
+                                                value={phoneNumber}
+                                                onChange={e => setFirstName(e.target.value)}
+                                                disabled={!editing} />
                                         </Form.Group>
                                     </Col>
 
                                     <Col>
-                                        <Form.Group controlId="formBirth">
-                                            <Form.Label>Date of Birth</Form.Label>
-                                            <Form.Control type="date" disabled={!editing} />
+                                        <Form.Group controlId={birthFormData.controlId}>
+                                            <Form.Label>{birthFormData.label}</Form.Label>
+                                            <Form.Control
+                                                type={birthFormData.type}
+                                                value={lastName}
+                                                onChange={e => setFirstName(e.target.value)}
+                                                disabled={!editing} />
                                         </Form.Group>
                                     </Col>
                                 </Row>
                             </Form>
+                        </Card.Body>
+                    </Accordion.Collapse>
+                </Card>
+            </Accordion>
+
+            <header className="AccordionHeader">Other Information</header>
+            <Accordion className='InfoAccordion'>
+                <Card className='AccordionCard'>
+                    <Accordion.Toggle as={Card.Body} eventKey="0" />
+                    <Accordion.Collapse eventKey="0">
+                        <Card.Body>
+                            Other information goes here.
                         </Card.Body>
                     </Accordion.Collapse>
                 </Card>
