5c57d31c60ecd3bdfa07d333a5d5bb402e08c30a
patrickbartlett <patrickbartlett@me.com>
2021-04-14 22:26:41 -0700

    added user profile to tab system

--- cloud-haven-react-app/src/components/TabSystem/TabSystem.js
+++ cloud-haven-react-app/src/components/TabSystem/TabSystem.js
@@ -4,15 +4,16 @@ import profile from "../../images/profile-user.png";
 import chatBubble from "../../images/chat.png";
 import calendar from "../../images/calendar.png";
 import "./TabSystem.css";
+import UserProfile from "../UserProfile/UserProfile";
 
 const TabSystem = () => {
     return (
         <Tabs defaultActiveKey="UserProfile" id="uncontrolled-tab-example" className="Tabs">
             <Tab eventKey="UserProfile" title={<span> <img className="TabIcon" src={profile} height="30"/> User Profile </span>}>
-                User profile goes here.
+                <UserProfile/>
             </Tab>
             <Tab eventKey="Messages" title={<span> <img className="TabIcon" src={chatBubble} height="30"/> Messages </span>}>
-                Messages goes here. 
+                Messages goes here.
             </Tab>
             <Tab eventKey="Calendar" title={<span> <img className="TabIcon" src={calendar} height="30"/> Calendar </span>}>
                 Calendar goes here.
